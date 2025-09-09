59beff8c7523571c44df0af84788d2c68d00af76
Jake Gill <jakegill@Mac.attlocal.net>
2025-09-09 10:10:33 -0400

    build: terraform state bucket partitions for staging and prod

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket  = "text-digest-terraform-state"
-    key     = "staging/terraform.tfstate"
+    key     = "${terraform.workspace}/terraform.tfstate"
     region  = "us-east-1"
     encrypt = true
   }
