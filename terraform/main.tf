36a217e675312edeff41985d51c18bdd3d6ff120
Jake Gill <jakegill@Mac.attlocal.net>
2025-09-09 10:00:05 -0400

    build: ensure s3 bucket always exists for terraform state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "text-digest-terraform-state"
-    key            = "staging/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-locks"
-    encrypt        = true
+    bucket  = "text-digest-terraform-state"
+    key     = "staging/terraform.tfstate"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
 
