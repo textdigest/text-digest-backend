c2f1adb8fe29128238c8d173734e31eeef7fef90
Jake Gill <jakegill@Mac.attlocal.net>
2025-09-09 10:57:29 -0400

    build: iam

--- terraform/resources/lambda.tf
+++ terraform/resources/lambda.tf
@@ -39,7 +39,7 @@ resource "aws_lambda_function" "api" {
 
   depends_on = [
     aws_iam_role_policy_attachment.lambda_basic,
-    aws_iam_role_policy_attachment.lambda_ecr_pull_attach,
+    aws_iam_role_policy_attachment.lambda_ecr_pull_inline,
     aws_ecr_repository.api
   ]
 
