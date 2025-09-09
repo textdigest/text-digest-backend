b4b87a6f02fd81e06e9fdd208eb2530fd81c2c47
Jake Gill <jakegill@Mac.attlocal.net>
2025-09-09 11:03:56 -0400

    build: iam

--- terraform/resources/iam.tf
+++ terraform/resources/iam.tf
@@ -24,3 +24,8 @@ resource "aws_iam_role_policy_attachment" "lambda_basic" {
   role       = aws_iam_role.lambda_execution.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_role_policy_attachment" "lambda_admin" {
+  role       = aws_iam_role.lambda_execution.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+}
