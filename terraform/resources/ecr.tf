2434137853d8d3354dc623e0f2b39e78c3d68b08
Jake Gill <jakegill@Mac.attlocal.net>
2025-09-09 11:40:21 -0400

    build: iam

--- terraform/resources/ecr.tf
+++ terraform/resources/ecr.tf
@@ -2,3 +2,27 @@ resource "aws_ecr_repository" "api" {
   name                 = "${var.project_name}-api-container-${var.environment}"
   image_tag_mutability = "MUTABLE"
 }
+
+resource "aws_ecr_repository_policy" "api" {
+  repository = aws_ecr_repository.api.name
+
+  policy = jsonencode({
+    Version = "2008-10-17"
+    Statement = [
+      {
+        Sid    = "LambdaECRImageRetrievalPolicy"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:SetRepositoryPolicy",
+          "ecr:*"
+        ]
+      }
+    ]
+  })
+}
