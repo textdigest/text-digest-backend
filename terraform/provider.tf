d959a42bfb12fd456f7bdce3b0f2deacd98620d9
Jake Gill <jakegill@Mac.attlocal.net>
2025-09-09 08:50:08 -0400

    build: cicd script

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,13 +5,11 @@ terraform {
       version = "~> 5.0"
     }
   }
-  
-  required_version = ">= 1.0"
 }
 
 provider "aws" {
   region = var.aws_region
-  
+
   default_tags {
     tags = {
       Environment = var.environment
