79f5e4663c233395210fef8088dc92925ee98bec
Jake Gill <jakegill@Mac.attlocal.net>
2025-09-09 15:11:46 -0400

    build: modify dynamodb pk and sk attribute names

--- terraform/resources/dynamodb.tf
+++ terraform/resources/dynamodb.tf
@@ -1,16 +1,16 @@
 resource "aws_dynamodb_table" "this" {
   name         = "${var.project_name}-${var.environment}-table"
   billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "itemId"
-  range_key    = "itemType"
+  hash_key     = "PK"
+  range_key    = "SK"
 
   attribute {
-    name = "itemId"
+    name = "PK"
     type = "S"
   }
 
   attribute {
-    name = "itemType"
+    name = "SK"
     type = "S"
   }
 }
