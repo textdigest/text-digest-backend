d6539637caf142e8bd3a1413681696cfdd844c60
Jake Gill <jakegill@Mac.attlocal.net>
2025-09-30 16:50:32 -0400

    refactor: add id token check to health endpoint to verify user

--- src/api/v1/endpoints/health.py
+++ src/api/v1/endpoints/health.py
@@ -1,7 +1,14 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Request, HTTPException
+from util.tokens.verifyIdToken import verify_token
 
 router = APIRouter()
 
 @router.get("/")
-async def health_check():
-    return {"message": "API is healthy."}
+async def health_check(request: Request):
+    auth_header = request.headers.get("authorization")
+    if not auth_header:
+        return {"message": "API is healthy."}
+
+    user_id = verify_token(auth_header)
+
+    return {"message": "API is healthy.", "user_id": user_id}
