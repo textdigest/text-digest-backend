8dbd673c467edf8867d44ae13ba33f692d67f0b3
Jake Gill <jakegill@Mac.attlocal.net>
2025-10-07 18:35:21 -0400

    build: add cors config to s3

--- terraform/resources/s3.tf
+++ terraform/resources/s3.tf
@@ -1,3 +1,9 @@
 resource "aws_s3_bucket" "this" {
   bucket = "${var.project_name}-${var.environment}-bucket"
+
+  cors_rule {
+    allowed_origins = ["https://localhost:3000", "https://app.textdigest.ai/"]
+    allowed_methods = ["GET", "HEAD"]
+    allowed_headers = ["*"]
+  }
 }
