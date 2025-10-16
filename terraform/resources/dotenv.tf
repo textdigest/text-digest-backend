e5c87ef6495f019583d092559dabb32b7fb8beb7
Jake Gill <jakegill@Mac.attlocal.net>
2025-10-16 16:02:48 -0400

    fix(ws): modify websocket url to include stage

--- terraform/resources/dotenv.tf
+++ terraform/resources/dotenv.tf
@@ -7,7 +7,7 @@ locals {
     ASYNC_PDF_EXTRACT_QUEUE_URL = aws_sqs_queue.async_pdf_extract.url
     POOL_ID                     = aws_cognito_user_pool.this.id
     CLIENT_ID                   = aws_cognito_user_pool_client.this.id
-    WEBSOCKET_API_GATEWAY       = aws_apigatewayv2_api.ws_api.api_endpoint
+    WEBSOCKET_API_GATEWAY       = "${aws_apigatewayv2_api.ws_api.api_endpoint}/${aws_apigatewayv2_stage.ws_stage.name}"
   }
 }
 
