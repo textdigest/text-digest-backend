bffe8377281004c7828d1380240ebb55cf72bff5
Jake Gill <jakegill@Jakes-Mac-mini.local>
2025-10-30 11:02:06 -0400

    build: s3 cors

--- terraform/resources/s3.tf
+++ terraform/resources/s3.tf
@@ -6,7 +6,7 @@ resource "aws_s3_bucket_cors_configuration" "this" {
   bucket = aws_s3_bucket.this.id
 
   cors_rule {
-    allowed_origins = ["http://localhost:3000", "https://app.textdigest.ai"]
+    allowed_origins = ["http://localhost:3000", "https://app.textdigest.ai", "https://staging.textdigest.ai"]
     allowed_methods = ["GET", "HEAD"]
     allowed_headers = ["*"]
     max_age_seconds = 3000
