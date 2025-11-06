6fa5f50bb138ac0a27e8b1f86ccef9f8731def84
Jake Gill <jakegill@Jakes-Mac-mini.local>
2025-11-06 10:55:08 -0500

    feat(reader): ai streams back response from queue

--- src/api/v1/endpoints/reader.py
+++ src/api/v1/endpoints/reader.py
@@ -2,14 +2,23 @@ from fastapi import APIRouter, Request
 from agents import TResponseInputItem
 from pydantic import BaseModel
 from typing import List
+from mypy_boto3_sqs.client import SQSClient
+
+import boto3
+import os
+import json
 
 from util.tokens.verifyIdToken import verify_token
 
-from services.agents.stream_run import stream_run
-from services.agents.qna_agent import qna_agent, QnaAgentContext
-from services.websocket.streamer import WebSocketStream
+from dotenv import load_dotenv
+load_dotenv()
 
 router = APIRouter()
+
+REGION = os.getenv("REGION") or ''
+ASYNC_AGENT_RUNNER_QUEUE_URL = os.getenv("ASYNC_AGENT_RUNNER_QUEUE_URL") or ''
+
+sqs_client: SQSClient = boto3.client("sqs", region_name=REGION)
 
 class PostQnaRequest(BaseModel):
     query: str
@@ -23,21 +32,20 @@ async def post_qna_message(request: Request, body: PostQnaRequest):
 
     user_id = verify_token(auth_header)
 
-    websocket_streamer = WebSocketStream("reader-qna", user_id)
-
-    stream_res = await stream_run(
-        agent=qna_agent,
-        input_items=body.curr_conversation,
-        stream_callback=websocket_streamer.send_chunk,
-        context=QnaAgentContext(
-            query=body.query,
-            highlighted_text=body.highlighted_text,
-            page_content=body.page_content,
-        ),
+    sqs_client.send_message(
+        QueueUrl=ASYNC_AGENT_RUNNER_QUEUE_URL,
+        MessageBody=json.dumps({
+            "agent_name": "qna_agent",
+            "agent_params": {
+                "curr_conversation": body.curr_conversation,
+                "query": body.query,
+                "highlighted_text": body.highlighted_text,
+                "page_content": body.page_content,
+            },
+            "user_id": user_id
+        }),
+        MessageGroupId=user_id,
+        MessageDeduplicationId=f"{user_id}-{hash(json.dumps(body.dict()))}"
     )
 
-    conversation = stream_res.to_input_list()
-
-    await websocket_streamer.send_chunk(conversation, "turn-over")
-
-    return {"conversation": conversation}
+    return {"ok": True}
